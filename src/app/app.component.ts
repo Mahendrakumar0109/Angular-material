6023fe5c8adfbdd0c3822f4de2e098f8a0aa6fd0
getmygitid <domain.getinfo@example.com>
2023-07-26 12:43:58 +0530

    Angular Material components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Components';
-}
+  /* showSpinner=false;
+  loadData(){
+    this.showSpinner=true;
+    setTimeout(()=>{
+      this.showSpinner=false
+    },5000)
+  } */
+
+  /* opened=false; */
+  
+  /* options:string[]=['angular','react','vue'];
+  objectOptions=[
+    {name:'Angular'},
+    {name:'Angular Material'},
+    {name:'React'},
+    {name:'Vue'}
+
+  ];
+  displayFn(subject: { name: any; }){
+    return subject ? subject.name:undefined;
+  } */
+
+  /* constructor(private snackBar:MatSnackBar){}
+    openSnackBar(message: string, action:string){
+      this.snackBar.open(message,action);
+    } */
+
+    
+  }
+
+ 
